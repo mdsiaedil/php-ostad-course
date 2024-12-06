87fddd8a2f4981d32d48c487d5c28f7151a5cab8
mdsiaedil <siaedilbasar@gmail.com>
2024-12-06 12:04:53 +0600

    variable,variable type, echo

---
+++ helloworld.php
@@ -0,0 +1,24 @@
+<?php
+//variable 
+ 
+$temperature = 20.33;
+
+
+echo "the temperature is ".$temperature;
+
+echo "\nMy name varialbe type is: ".gettype($temperature);
+
+
+
+/* $price = 30.24;
+$age = 28; 
+ */
+
+
+
+
+
+
+/* echo "My name is Siaedil Basar";
+echo "\n";
+echo "I am a university student"; */
